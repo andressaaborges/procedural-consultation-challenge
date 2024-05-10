056591767ab076b3bfd33c4940642cf8d0097420
andressaaborges <emylleaborges@gmail.com>
2024-05-10 17:18:11 -0300

    test: :white_check_mark: add and update e2e tests

--- cypress/e2e/process-display.cy.ts
+++ cypress/e2e/process-display.cy.ts
@@ -2,13 +2,21 @@ describe('process display', () => {
   it('should be able to display data from an existing process', () => {
     cy.searchByQuery('8500235-12.2019.4.05.8000')
 
-    cy.location('pathname').should('include', '/process')
-
     cy.location('pathname').should(
       'equal',
       '/process/8500235-12.2019.4.05.8000',
     )
 
     cy.contains('Processo nº 8500235-12.2019.4.05.8000 do TRF5')
+  })
+
+  it('should be able to display an error message when the process number is incorrect', () => {
+    cy.searchByQuery('123456789')
+
+    cy.location('pathname').should('include', '/')
+
+    cy.contains(
+      'Número inválido! CNJ deve seguir o formato: 0000000-00.0000.0.00.0000',
+    )
   })
 })

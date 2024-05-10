d8e2490491c49c2ffc885b6195ee130cb4ddbaa9
andressaaborges <emylleaborges@gmail.com>
2024-05-10 20:36:19 -0300

    feat: :safety_vest: add error message when not find cnj in the API

--- src/app/(cases)/process/[cnj]/page.tsx
+++ src/app/(cases)/process/[cnj]/page.tsx
@@ -15,6 +15,11 @@ async function getProcess(cnj: string): Promise<Case> {
     },
   })
 
+  if (!response.ok) {
+    const errorData = await response.json()
+    throw new Error(errorData.message || 'Erro desconhecido')
+  }
+
   const caseItem = await response.json()
 
   return caseItem
@@ -29,57 +34,61 @@ export async function generateMetadata({
 }
 
 export default async function ProcessPage({ params }: ProcessProps) {
-  const caseItem = await getProcess(params.cnj)
+  try {
+    const caseItem = await getProcess(params.cnj)
 
-  return (
-    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
-      <div className="col-span-6 row-span-6 flex flex-col gap-10">
-        <h2>
-          Processo nº {caseItem.cnj} do {caseItem.court}
-        </h2>
-        <div className="flex flex-col">
-          <h4 className="rounded-se-lg rounded-ss-lg bg-zinc-900 border-b-4 border-zinc-950 p-4 font-semibold">
-            Movimentações
-          </h4>
-          {caseItem.movements.map((movement) => (
-            <div
-              key={movement.date.concat(movement.description)}
-              className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4"
-            >
-              <p>
-                <span>{movement.date}</span>
-                <br />
-                {movement.description}
-              </p>
+    return (
+      <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+        <div className="col-span-6 row-span-6 flex flex-col gap-10">
+          <h2>
+            Processo nº {caseItem.cnj} do {caseItem.court}
+          </h2>
+          <div className="flex flex-col">
+            <h4 className="rounded-se-lg rounded-ss-lg bg-zinc-900 border-b-4 border-zinc-950 p-4 font-semibold">
+              Movimentações
+            </h4>
+            {caseItem.movements.map((movement) => (
+              <div
+                key={movement.date.concat(movement.description)}
+                className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4"
+              >
+                <p>
+                  <span>{movement.date}</span>
+                  <br />
+                  {movement.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="col-span-3 row-span-3 flex flex-col gap-10">
+          <h2>Mais informações</h2>
+
+          <div className="flex flex-col">
+            <h4 className="rounded-se-lg rounded-ss-lg bg-zinc-900 border-b-4 border-zinc-950 p-4 font-semibold">
+              Detalhes do processo
+            </h4>
+            <div className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4">
+              <p>{caseItem.details}</p>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
 
-      <div className="col-span-3 row-span-3 flex flex-col gap-10">
-        <h2>Mais informações</h2>
-
-        <div className="flex flex-col">
+        <div className="col-span-3 row-span-3 flex flex-col gap-0">
           <h4 className="rounded-se-lg rounded-ss-lg bg-zinc-900 border-b-4 border-zinc-950 p-4 font-semibold">
-            Detalhes do processo
+            Partes envolvidas
           </h4>
-          <div className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4">
-            <p>{caseItem.details}</p>
+          <div className="flex flex-col">
+            <div className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4">
+              <p>Autor: {caseItem.parties.plaintiff}</p>
+              <p>Réu: {caseItem.parties.defendant}</p>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="col-span-3 row-span-3 flex flex-col gap-0">
-        <h4 className="rounded-se-lg rounded-ss-lg bg-zinc-900 border-b-4 border-zinc-950 p-4 font-semibold">
-          Partes envolvidas
-        </h4>
-        <div className="flex flex-col">
-          <div className="bg-zinc-400 border-b-4 border-zinc-950 text-zinc-950 p-4">
-            <p>Autor: {caseItem.parties.plaintiff}</p>
-            <p>Réu: {caseItem.parties.defendant}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
+  } catch (error) {
+    return <p>{(error as Error).message}</p>
+  }
 }

ed9fac429dddfb6cde35fdae157746dd2dd94be3
andressaaborges <emylleaborges@gmail.com>
2024-05-10 17:14:59 -0300

    feat: :safety_vest: add format cnj number validation

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,40 +1,59 @@
 'use client'
 
+import { cnjRegexValidation } from '@/utils/validation'
 import { Search } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { FormEvent } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const CnjFormSchema = z.object({
+  cnj: z.string().regex(cnjRegexValidation, {
+    message:
+      'Número inválido! CNJ deve seguir o formato: 0000000-00.0000.0.00.0000',
+  }),
+})
+
+type CnjFormData = z.infer<typeof CnjFormSchema>
 
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CnjFormData>({
+    resolver: zodResolver(CnjFormSchema),
+  })
+
   const router = useRouter()
-
-  function handleSearch(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
-    const formData = new FormData(e.currentTarget)
-    const data = Object.fromEntries(formData)
-    const query = data.q
-
-    if (!query) {
-      return null
-    }
-
-    router.push(`/process/${query}`)
+  async function onSubmit(data: CnjFormData) {
+    router.push(`/process/${data.cnj}`)
   }
 
   return (
-    <form
-      onSubmit={handleSearch}
-      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
-    >
-      <Search className="w-6 h-5 text-zinc-500" />
+    <div className="flex flex-col items-center justify-items-center gap-10">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+      >
+        <Search className="w-6 h-5 text-zinc-500" />
 
-      <input
-        name="q"
-        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-        placeholder="Digite o número de processo..."
-        required
-      />
+        <input
+          type="text"
+          {...register('cnj')}
+          className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+          placeholder="Digite o número de processo..."
+          required
+        />
 
-      <button type="submit">Buscar</button>
-    </form>
+        <button type="submit">Buscar</button>
+      </form>
+
+      <p>
+        {errors.cnj
+          ? errors.cnj.message
+          : 'Digite o número CNJ do processo com números, hífen e pontos para realizar a busca!'}
+      </p>
+    </div>
   )
 }
